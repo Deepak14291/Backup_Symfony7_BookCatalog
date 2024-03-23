b5b77f173378ce7c7a428de08a376a1876f11856
Deepak Kodi <125197608+Deepak14291@users.noreply.github.com>
2024-03-23 23:37:16 +0000

    Symfony 7 first commit

---
+++ src/Entity/Books.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BooksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BooksRepository::class)]
+class Books
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $book_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $book_author = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $book_genre = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBookName(): ?string
+    {
+        return $this->book_name;
+    }
+
+    public function setBookName(string $book_name): static
+    {
+        $this->book_name = $book_name;
+
+        return $this;
+    }
+
+    public function getBookAuthor(): ?string
+    {
+        return $this->book_author;
+    }
+
+    public function setBookAuthor(string $book_author): static
+    {
+        $this->book_author = $book_author;
+
+        return $this;
+    }
+
+    public function getBookGenre(): ?string
+    {
+        return $this->book_genre;
+    }
+
+    public function setBookGenre(string $book_genre): static
+    {
+        $this->book_genre = $book_genre;
+
+        return $this;
+    }
+}
